b9ecae87c9f09f84ee41b00e405780b6d6330069
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-11 11:05:18 +0200

    creation classe counter avec constructeur + methode

---
+++ src/compteur/Counter.java
@@ -0,0 +1,46 @@
+package compteur;
+
+public class Counter 
+{
+	private int position;
+	private int step;
+	
+	public Counter() 
+	{
+		this.position = 0;
+		this.step = 1;
+	}
+
+	public int getValue() 
+	{
+		return position;
+	}
+	
+	public void inc()
+	{
+		this.position += this.step; 
+	}
+	
+	public boolean equals(Counter c)
+	{
+		boolean result = false;
+		if (this.position == c.getValue())
+		{
+			result = true;
+		}
+
+		return result;
+		
+	}
+
+	public String toString() 
+	{
+		return "valeur : " + position + " (incrémente" + step + ")";
+	}
+	
+	
+	
+	
+	
+
+}
