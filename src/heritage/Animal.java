bd7d2ee7e22132f9432eecc8fd00cfe27411e9bf
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-12 10:31:56 +0200

    creation package animal

--- src/heritage/Animal.java
+++
@@ -1,5 +0,0 @@
-package heritage;
-
-public class Animal {
-
-}
---
+++ src/heritage/animal/Animal.java
@@ -0,0 +1,14 @@
+package heritage.animal;
+
+public abstract class Animal 
+{
+	private int age;
+	private int poids;
+	private int taille;
+	private String nom;
+	
+	public abstract void manger();
+	
+	public abstract void deplacer();
+
+}
