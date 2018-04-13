55491219620b53f3dba0fad3f6cd301938568615
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-13 16:33:17 +0200

    ajout package multithread ajout class counter et main

---
+++ src/multithread/Counter.java
@@ -0,0 +1,15 @@
+package multithread;
+
+public class Counter implements Runnable 
+{
+	public void run() 
+	{
+		int counter = 0;
+		while (counter < 10) 
+		{
+			System.out.print(counter + " ");
+			counter++;
+		}
+	}
+}
+
