947f573df4b21e0923885e64fb3224373f0c41e5
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-13 14:56:42 +0200

    changement methode to string

--- src/arraylist/User.java
+++ src/arraylist/User.java
@@ -1,48 +1,59 @@
 package arraylist;
 
-public class User  implements Comparable<User>{
+public class User  implements Comparable<User>
+{
 	private String name;
 	private Integer age;
 	private boolean isMale;
 
-	public User(String name, Integer age, boolean isMale) {
+	public User(String name, Integer age, boolean isMale) 
+	{
 		this.name = name;
 		this.age = age;
 		this.isMale = isMale;
 	}
 
-	public String getName() {
+	public String getName() 
+	{
 		return this.name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 
-	public Integer getAge() {
+	public Integer getAge() 
+	{
 		return this.age;
 	}
 
-	public void setAge(Integer age) {
+	public void setAge(Integer age) 
+	{
 		this.age = age;
 	}
 
-	public boolean isMale() {
+	public boolean isMale() 
+	{
 		return this.isMale;
 	}
 
-	public void setMale(boolean isMale) {
+	public void setMale(boolean isMale) 
+	{
 		this.isMale = isMale;
 	}
+	
 	
 	public int compareTo(User user2)
     {
         return this.getName().compareTo(user2.getName());
     }
 
-	public String toString() {
-		String sexe = this.isMale ? "un homme" : "une femme";
-		return "L'utilisateur se nomme " + this.name + ", il a " + this.age + " ans, c'est " + sexe;
+	public String toString() 
+	{
+		
+		return (this.isMale) ? "L'utilisateur se nomme " + this.name + ", il a " + this.age + " ans, c'est un homme" : "L'utilisatrice se nomme " + this.name + ", elle a " + this.age + " ans, c'est une femme";
+		
 	}
 
 }
