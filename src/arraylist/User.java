54d1968c4accb164aad1fd37eabb237da20238db
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-13 13:42:05 +0200

    implémentation class user

--- src/arraylist/User.java
+++ src/arraylist/User.java
@@ -1,5 +1,46 @@
 package arraylist;
 
-public class User {
+public class User 
+{
+	private String name;
+	private Integer age;
+	private boolean isMale;
+	
+	public User(String name, Integer age, boolean isMale) {
+		this.name = name;
+		this.age = age;
+		this.isMale = isMale;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public boolean isMale() {
+		return this.isMale;
+	}
+
+	public void setMale(boolean isMale) {
+		this.isMale = isMale;
+	}
+
+	public String toString() {
+		String sexe = this.isMale ? "un homme"  : "une femme";
+		return "L'utilisateur se nomme " + this.name + ", il a " + this.age + "ans, c'est " + sexe;
+	}
+	
+	
 
 }
